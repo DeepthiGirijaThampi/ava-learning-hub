4a5e12ee06701d9cbf61d58d154c1bd033165e1d
DeepthiGirijaThampi <deepthig01@gmail.com>
2025-11-04 09:59:41 -0600

    Updated the About and Profile components and the App.js file accordingly

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,13 @@
+import Header from "../common/Header";
+import Footer from "../common/Footer";
+export default function About(){
+    return(
+        <>
+        <Header></Header>
+        <main>
+             <h1 style={{display:"flex",justifyContent:"center"}}>About Page</h1>
+        </main>
+        <Footer></Footer>
+        </>
+    )
+}
---
+++ src/components/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import Header from "../common/Header";
+import Footer from "../common/Footer";
+export default function Profile(){
+    return(
+        <>
+        <Header></Header>
+        <main>
+        <h1 style={{display:"flex",justifyContent:"center"}}>Profile Page</h1>
+        </main>
+        <Footer></Footer>
+        </>
+    )
+}
