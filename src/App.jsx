6628b935192c3a19bc0c1caa9346dd177c7e4e64
DeepthiGirijaThampi <deepthig01@gmail.com>
2025-11-03 21:14:48 -0600

    Created components folder, common and pages folder inside it and Added required pages and common jsx files to make the initial folder stucture

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-
+import { Routes, Route } from "react-router"
+import Home from "./components/pages/Home"
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+export default function Home(){
+    return(
+        <>
+     
+        <main>
+         <h1 style={{display:"flex",justifyContent:"center"}}>Home Page</h1>
+        </main>
+     
+        </>
+    )
+}
