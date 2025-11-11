d7a0631965c74a0efc1259d5434551068f7a5c96
DeepthiGirijaThampi <deepthig01@gmail.com>
2025-11-10 18:03:06 -0600

    Added initial structure to the Home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,19 @@
-
+import { useNavigate ,Link } from "react-router";
 export default function Home(){
     return(
-        <>
-     
-        <main>
-         <h1 style={{display:"flex",justifyContent:"center"}}>Home Page</h1>
+        <main className="home-page">
+        <div className="welcome">
+         <h1 style={{display:"flex",justifyContent:"center",color:"purple"}}>Welcome to Ava’s Learning Hub</h1>
+         <p style={{display:"flex",justifyContent:"center", color:"purple"}}>Your personal space to explore, reflect, and grow 🌱</p>
+         
+         </div>
+         <section className="features">
+        <Link to={"/subjects"}><div className="feature-card">📚 Track Subjects</div></Link>
+        <Link to={"/reflections"}><div className="feature-card">✍️ Reflect on Learning</div></Link>
+        <Link to={"/profile"}><div className="feature-card">📈 Celebrate Progress</div></Link>
+         <div className="feature-card">🌍 Coming soon: multi-user!</div>
+        </section>
+        <button onClick={() => navigate("/subjects")} className="start-learning-button">Get Started</button>
         </main>
-
-        </>
     )
 }
