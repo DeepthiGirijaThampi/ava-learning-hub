6e42e48ae1f1cb3c5b217df86d1371ab09303f1b
DeepthiGirijaThampi <deepthig01@gmail.com>
2025-11-10 19:02:45 -0600

    Updated the structure for the Reflections page

--- src/components/pages/Reflections.jsx
+++ src/components/pages/Reflections.jsx
@@ -1,11 +1,19 @@
 export default function Reflections(){
     return(
-        <>
-       
+      
         <main>
         <h1 style={{display:"flex",justifyContent:"center"}}>Reflections Page</h1>
+        <p style={{textAlign:"center"}}>
+            Write your thoughts about what you learned today ✍
+        </p>
+        {/* placeholder for reflection entries*/ }
+        <div style={{textAlign:"center",margin:"1rem",color:"gray"}}>
+            <p>No reflections yet...</p>
+        </div>
+        <div style={{textAlign:"center"}}>
+            <button>Add Reflections</button>
+        </div>
         </main>
       
-        </>
     )
 }
