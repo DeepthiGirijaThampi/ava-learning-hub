894e789f5573419625b474f4031d016b988a369f
DeepthiGirijaThampi <deepthig01@gmail.com>
2025-11-10 18:13:53 -0600

    Updated the About page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,12 +1,18 @@
 
 export default function About(){
     return(
-        <>
-
-        <main>
-             <h1 style={{display:"flex",justifyContent:"center"}}>About Page</h1>
+        <main >
+         <section >
+         <h2>About Ava’s Learning Hub</h2>
+         <p><em>Created by <strong >Deepthi Girija Thampi</strong>, as part of a solo project to inspire young learners like Ava 🧡</em></p>
+            <p>
+             This solo project is a React-based web application built with love and creativity — and will grow to support
+             multiple users in the future.
+             </p>
+             <p>
+              Current features include tracking subjects, units, reflections, and more. Stay tuned for future enhancements!
+             </p>
+            </section>
         </main>
-      
-        </>
     )
 }
