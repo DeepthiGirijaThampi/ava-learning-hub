54bd8c5223999e36c48355b593f9fafad08fa80b
DeepthiGirijaThampi <deepthig01@gmail.com>
2025-11-15 22:30:52 -0600

    Updated NavMenu.jsx to get a hamburger style while in mobile device and added nav-menu.css and updatde index.css to hide the hamburger in all other devices

--- src/components/common/NavMenu.jsx
+++ src/components/common/NavMenu.jsx
@@ -1,13 +1,24 @@
 import { Link } from "react-router";
+import { useState } from 'react';
 
 export default function NavMenu(){
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () =>{
+        setMenuOpen(!menuOpen)
+    };
 return(
-    <div className="nav-menu">
+   <nav className="nav-menu" >
+   <button className="hamburger" onClick={toggleMenu}>
+   ☰
+   </button>
+    <div className={`menu-links ${menuOpen ? 'show' :''}`}>
         <Link className="link" to={"/"}>Home  </Link>
         <Link className="link" to={"/about"}>About </Link>
         <Link className="link" to={"/profile"}>Profile </Link>
         <Link className="link" to={"/subjects"}>Subjects </Link>
         <Link className="link" to={"/reflections"}>Reflections </Link>
     </div>
+   </nav>  
 )
 }
