e44c9de64fadc52b3185c6e220f0eb7b0d172733
DeepthiGirijaThampi <deepthig01@gmail.com>
2025-11-15 22:38:55 -0600

    Updated a new styling in navmenu

--- src/components/common/NavMenu.jsx
+++ src/components/common/NavMenu.jsx
@@ -4,21 +4,22 @@ import { useState } from 'react';
 export default function NavMenu(){
     const [menuOpen, setMenuOpen] = useState(false);
 
-    const toggleMenu = () =>{
-        setMenuOpen(!menuOpen)
-    };
-return(
-   <nav className="nav-menu" >
-   <button className="hamburger" onClick={toggleMenu}>
-   ☰
-   </button>
-    <div className={`menu-links ${menuOpen ? 'show' :''}`}>
-        <Link className="link" to={"/"}>Home  </Link>
-        <Link className="link" to={"/about"}>About </Link>
-        <Link className="link" to={"/profile"}>Profile </Link>
-        <Link className="link" to={"/subjects"}>Subjects </Link>
-        <Link className="link" to={"/reflections"}>Reflections </Link>
-    </div>
-   </nav>  
-)
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  };
+    return(
+    <nav className="nav-menu">
+      <button className="hamburger" onClick={toggleMenu}>
+        ☰
+      </button>
+
+      <div className={`menu-links ${menuOpen ? 'show' : ''}`}>
+        <Link className="link" id="links" to="/" onClick={() => setMenuOpen(false)} >Home  </Link>
+        <Link className="link" id="links" to="/about" onClick={() => setMenuOpen(false)} >About  </Link>
+        <Link className="link" id="links" to="/profile" onClick={() => setMenuOpen(false)} >Profile  </Link>
+        <Link className="link" id="links" to="/reflections" onClick={() => setMenuOpen(false)} >Reflections  </Link>
+        <Link className="link" id="links" to="/subjects" onClick={() => setMenuOpen(false)} >Subjects  </Link>
+      </div>
+    </nav>
+    )
 }
