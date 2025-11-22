46c636400f49756536d9a8357f121937561c6dbd
DeepthiGirijaThampi <deepthig01@gmail.com>
2025-11-21 19:21:13 -0600

    Updated the header styling and footer styling

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer(){
     let thisYear = new Date().getFullYear();
     return(
         <footer>
-            <div style={{color:"#3a5a40"}}>&copy; {thisYear} Ava's Learning Hub private limited</div>
+            <div style={{color:"#3a5a40"}}><strong>&copy; {thisYear} Ava's Learning Hub private limited</strong></div>
         </footer>
     )
 }
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -5,7 +5,7 @@ import NavMenu from "./NavMenu";
 export default function Header(){
     return(
         <header>
-            <h1 className="main-heading" style={{color:"#3a5a40"}}>Ava's Learning Hub</h1>
+            <h1 className="main-heading" >Ava's Learning Hub</h1>
             <NavMenu></NavMenu>
         </header>
     )
